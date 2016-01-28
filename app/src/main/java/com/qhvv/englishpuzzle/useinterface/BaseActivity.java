4de5232078ea4d32c8eb7c83465b285f38c19560
Vo Quang Hoa <voquanghoa@gmail.com>
2016-01-28 15:36:36 +0700

    - Implement ads - Delete unused files

--- app/src/main/java/com/qhvv/englishpuzzle/useinterface/BaseActivity.java
+++ app/src/main/java/com/qhvv/englishpuzzle/useinterface/BaseActivity.java
@@ -7,7 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 import com.qhvv.englishpuzzle.R;
 import com.qhvv.englishpuzzle.configuration.IWebService;
@@ -18,11 +24,22 @@ import com.qhvv.englishpuzzle.configuration.IWebService;
 public class BaseActivity extends Activity implements IWebService, DialogInterface.OnCancelListener {
     private ProgressDialog progressDialog;
 
+    private AdView adView;
+    private static AdRequest adRequest;
+    protected InterstitialAd mInterstitialAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if(adRequest == null){
+            adRequest = createAdsRequest();
+        }
+    }
+
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        loadBannerAds();
     }
 
     protected  void showLoadingDialog(){
@@ -37,6 +54,42 @@ public class BaseActivity extends Activity implements IWebService, DialogInterfa
         if(progressDialog!=null){
             progressDialog.hide();
         }
+    }
+
+    protected void loadBannerAds(){
+        RelativeLayout adsLayout = (RelativeLayout)findViewById(R.id.adView);
+        if(adsLayout!=null){
+            String AD_UNIT_ID = getString(R.string.admob_id);
+            adView = new AdView(this);
+            adView.setAdSize(AdSize.BANNER);
+            adView.setAdUnitId(AD_UNIT_ID);
+            adsLayout.addView(adView);
+            adView.loadAd(adRequest);
+        }
+    }
+
+    protected void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("EF966C3E6FD639F322B1250C72187DF5")
+                .addTestDevice("E62072DEC66B8E891FC23264834F5CCA")
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    protected void loadFullAds(){
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.admob_id_full_screen));
+        requestNewInterstitial();
+    }
+
+    protected AdRequest createAdsRequest(){
+        return new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("EF966C3E6FD639F322B1250C72187DF5")
+                .addTestDevice("E62072DEC66B8E891FC23264834F5CCA")
+                .build();
     }
 
     public void onCancel(DialogInterface dialog) {
